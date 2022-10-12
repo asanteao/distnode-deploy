386d8345f74de83a6eda86892391957fd3827e12
aasante <aasante7801@hotmail.com>
2022-10-12 19:35:18 -0400

    Enable Heroku deployment

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM Node:17.8.0-alpine3.12
+WORKDIR /srv
+COPY package*.json ./
+RUN npm ci --only=production
+COPY . .
+ENV HOST=0.0.0.0
+CMD [ "node", "server.js" ]
---
+++ deploy-heroku.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget -q0- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku plugins:install @heroku-cli/plugin-container-registry
+heroku container:login
+heroku container:push web --app asanteao-distnode
+heroku container:release web --app asanteao-distnode
